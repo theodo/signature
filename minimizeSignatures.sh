e638784c92955aedafebda4b19c769336e53c548
Gireg <gireg.dekerdanet@hotmail.fr>
2018-05-06 19:42:42 +0100

    add links in m33 signature

--- minimizeSignatures.sh
+++ minimizeSignatures.sh
@@ -2,4 +2,4 @@ htmlmin -o signatureTemplates/bam-signature.min.html signatureTemplates/bam-sign
 htmlmin -o signatureTemplates/theodo-signature.min.html signatureTemplates/theodo-signature.html
 htmlmin -o signatureTemplates/m33-signature.min.html signatureTemplates/m33-signature.html
 echo 'signature templates minimized'
-echo 'You need to update signature dates in loadSignatureTemplate.js'
+echo 'You need to update signature update dates in config.js'
