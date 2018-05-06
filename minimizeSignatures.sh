b3fb1a470594bb825888acfeb0c973d5854b4ecb
Gireg <gireg.dekerdanet@hotmail.fr>
2018-05-06 17:35:35 +0100

    Create M33 signature

--- minimizeSignatures.sh
+++ minimizeSignatures.sh
@@ -1,4 +1,5 @@
 htmlmin -o signatureTemplates/bam-signature.min.html signatureTemplates/bam-signature.html
 htmlmin -o signatureTemplates/theodo-signature.min.html signatureTemplates/theodo-signature.html
+htmlmin -o signatureTemplates/m33-signature.min.html signatureTemplates/m33-signature.html
 echo 'signature templates minimized'
 echo 'You need to update signature dates in loadSignatureTemplate.js'
