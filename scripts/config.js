c2e17f69e5b7e5e898d544a79194397718c3ba26
Gireg <gireg.dekerdanet@hotmail.fr>
2018-11-26 23:49:36 +0100

    add padok signature

--- scripts/config.js
+++ scripts/config.js
@@ -1,29 +1,38 @@
 var config = {
-  "Theodo": {
-    "signatureTemplate": "signatureTemplates/theodo-signature.min.html",
-    "lastUpdate": new Date('6/26/2018'), //Month/Day/Year
-    "logo": "images/logoTheodo.png",
-    "form": {
-      "mail": false,
-      "country": true
+  Theodo: {
+    signatureTemplate: "signatureTemplates/theodo-signature.min.html",
+    lastUpdate: new Date("6/26/2018"), //Month/Day/Year
+    logo: "images/logoTheodo.png",
+    form: {
+      mail: false,
+      country: true
     }
   },
-  "BAM": {
-    "signatureTemplate": "signatureTemplates/bam-signature.min.html",
-    "lastUpdate": new Date('4/22/2018'),
-    "logo": "images/bamLogo.jpg",
-    "form": {
-      "mail": true,
-      "country": false
+  BAM: {
+    signatureTemplate: "signatureTemplates/bam-signature.min.html",
+    lastUpdate: new Date("4/22/2018"),
+    logo: "images/bamLogo.jpg",
+    form: {
+      mail: true,
+      country: false
     }
   },
-  "M33": {
-    "signatureTemplate": "signatureTemplates/m33-signature.min.html",
-    "lastUpdate": new Date('5/6/2018'),
-    "logo": "images/M33Logo.png",
-    "form": {
-      "mail": false,
-      "country": false
+  M33: {
+    signatureTemplate: "signatureTemplates/m33-signature.min.html",
+    lastUpdate: new Date("5/6/2018"),
+    logo: "images/M33Logo.png",
+    form: {
+      mail: false,
+      country: false
+    }
+  },
+  Padok: {
+    signatureTemplate: "signatureTemplates/padok-signature.html",
+    lastUpdate: new Date("11/26/2018"),
+    logo: "images/logoPadok.png",
+    form: {
+      mail: true,
+      country: false
     }
   }
-}
+};
