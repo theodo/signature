027371b555bd5291a5cb61911fe5c21812723dbc
Hugo Lime <ligohu@outlook.fr>
2019-02-22 14:02:22 +0100

    Prettify Sicara signature

--- scripts/config.js
+++ scripts/config.js
@@ -37,10 +37,10 @@ var config = {
   },
   Sicara: {
     signatureTemplate: "signatureTemplates/sicara-signature.min.html",
-    lastUpdate: new Date("01/31/2019"),
+    lastUpdate: new Date("02/22/2019"),
     logo: "images/logoSicara.png",
     form: {
-      mail: true,
+      mail: false,
       country: false
     }
   }
