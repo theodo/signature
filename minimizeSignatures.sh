ed3fe12fcaebd2d7231bfc104198547bb90b6471
Thibaud Lemaire <thibaudl@theodo.fr>
2024-06-22 01:15:09 +0200

    Add all Komyo signatures

--- minimizeSignatures.sh
+++ minimizeSignatures.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 htmlmin -o signatureTemplates/aleios-signature.min.html signatureTemplates/aleios-signature.html
-htmlmin -o signatureTemplates/theodo-healthtech-signature.min.html signatureTemplates/theodo-healthtech-signature.html
 htmlmin -o signatureTemplates/theodo-signature.min.html signatureTemplates/theodo-signature.html
+htmlmin -o signatureTemplates/theodo-healthtech-signature.min.html signatureTemplates/theodo-healthtech-signature.html
+htmlmin -o signatureTemplates/theodo-apps-signature.min.html signatureTemplates/theodo-apps-signature.html
+htmlmin -o signatureTemplates/theodo-cloud-signature.min.html signatureTemplates/theodo-cloud-signature.html
+htmlmin -o signatureTemplates/theodo-data-ai-signature.min.html signatureTemplates/theodo-data-ai-signature.html
+htmlmin -o signatureTemplates/theodo-fintech-signature.min.html signatureTemplates/theodo-fintech-signature.html
+htmlmin -o signatureTemplates/theodo-govtech-signature.min.html signatureTemplates/theodo-govtech-signature.html
 echo 'signature templates minimized'
 echo 'You need to update signature update dates in config.js'
--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 var config = {
-  TheodoNew: {
-    signatureTemplate: "signatureTemplates/theodo-signature.html",
+  Theodo: {
+    signatureTemplate: "signatureTemplates/theodo-signature.min.html",
     lastUpdate: new Date("06/16/2024"),
     logo: "images/logoTheodoNew.png",
     form: {
@@ -9,9 +9,54 @@ var config = {
     }
   },
   TheodoHealthTech: {
-    signatureTemplate: "signatureTemplates/theodo-healthtech-signature.html",
+    signatureTemplate: "signatureTemplates/theodo-healthtech-signature.min.html",
     lastUpdate: new Date("06/16/2024"),
     logo: "images/logoTheodoHealthTech.png",
+    form: {
+      mail: false,
+      country: true
+    }
+  },
+  TheodoDataAi: {
+    signatureTemplate: "signatureTemplates/theodo-data-ai-signature.min.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoDataAi.png",
+    form: {
+      mail: false,
+      country: true
+    }
+  },
+  TheodoFinTech: {
+    signatureTemplate: "signatureTemplates/theodo-fintech-signature.min.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoFinTech.png",
+    form: {
+      mail: false,
+      country: true
+    }
+  },
+  TheodoCloud: {
+    signatureTemplate: "signatureTemplates/theodo-cloud-signature.min.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoCloud.png",
+    form: {
+      mail: false,
+      country: true
+    }
+  },
+  TheodoApps: {
+    signatureTemplate: "signatureTemplates/theodo-apps-signature.min.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoApps.png",
+    form: {
+      mail: false,
+      country: true
+    }
+  },
+  TheodoGovTech: {
+    signatureTemplate: "signatureTemplates/theodo-govtech-signature.min.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoGovTech.png",
     form: {
       mail: false,
       country: true
