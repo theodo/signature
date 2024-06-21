38edfdeaefdd101310c580326d4061067b7f7392
cyprienraf <cyprienr@theodo.fr>
2024-06-21 16:43:07 +0200

    delete outdated signatures

--- scripts/config.js
+++ scripts/config.js
@@ -1,24 +1,6 @@
 var config = {
-  Theodo: {
-    signatureTemplate: "signatureTemplates/theodo-signature.min.html",
-    lastUpdate: new Date("01/16/2024"), //Month/Day/Year
-    logo: "images/logoTheodo.png",
-    form: {
-      mail: false,
-      country: true
-    }
-  },
-  TheodoHealthTech: {
-    signatureTemplate: "signatureTemplates/theodo-healthtech-signature.html",
-    lastUpdate: new Date("06/16/2024"),
-    logo: "images/logoTheodoHealthTech.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
   TheodoNew: {
-    signatureTemplate: "signatureTemplates/theodo-new.html",
+    signatureTemplate: "signatureTemplates/theodo.html",
     lastUpdate: new Date("06/16/2024"),
     logo: "images/logoTheodoNew.png",
     form: {
@@ -26,64 +8,10 @@ var config = {
       country: false
     }
   },
-  BAM: {
-    signatureTemplate: "signatureTemplates/bam-signature.min.html",
-    lastUpdate: new Date("8/18/2023"),
-    logo: "images/bamLogo2.png",
-    form: {
-      mail: true,
-      country: false
-    }
-  },
-  M33: {
-    signatureTemplate: "signatureTemplates/m33-signature.min.html",
-    lastUpdate: new Date("5/6/2018"),
-    logo: "images/M33Logo.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Padok: {
-    signatureTemplate: "signatureTemplates/padok-signature.min.html",
-    lastUpdate: new Date("11/17/2021"),
-    logo: "images/logoPadok.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Sicara: {
-    signatureTemplate: "signatureTemplates/sicara-signature.min.html",
-    lastUpdate: new Date("06/11/2019"),
-    logo: "images/logoSicara.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Sipios: {
-    signatureTemplate: "signatureTemplates/sipios-signature.min.html",
-    lastUpdate: new Date("11/02/2020"),
-    logo: "images/logo-sipios.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Hokla: {
-    signatureTemplate: "signatureTemplates/hokla-signature.min.html",
-    lastUpdate: new Date("11/15/2021"), //Month/Day/Year
-    logo: "images/logoHokla.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Kumo: {
-    signatureTemplate: "signatureTemplates/kumo-signature.min.html",
-    lastUpdate: new Date("07/23/2021"), //Month/Day/Year
-    logo: "images/logoKumo.png",
+  TheodoHealthTech: {
+    signatureTemplate: "signatureTemplates/theodo-healthtech-signature.html",
+    lastUpdate: new Date("06/16/2024"),
+    logo: "images/logoTheodoHealthTech.png",
     form: {
       mail: false,
       country: false
@@ -98,22 +26,4 @@ var config = {
       country: true
     }
   },
-  M33Foundation: {
-    signatureTemplate: "signatureTemplates/m33-foundation-signature.min.html",
-    lastUpdate: new Date("05/11/2023"), //Month/Day/Year
-    logo: "images/M33FoundationLogo.png",
-    form: {
-      mail: false,
-      country: false
-    }
-  },
-  Solona: {
-    signatureTemplate: "signatureTemplates/solona-signature.min.html",
-    lastUpdate: new Date("23/06/2023"), //Month/Day/Year
-    logo: "images/logoSolona.png",
-    form: {
-      mail: true,
-      country: false
-    }
-  }
 };
